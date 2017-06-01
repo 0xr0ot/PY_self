6b4b6d3657c7cdd07fcd6e412362d01e8607f3c0
Shinalone <shinalone@outlook.com>
2017-06-01 19:26:57 +0800

    Create sim_cos.py

---
+++ ML/recommend/sim_cos.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+#-*-coding: utf-8-*-
+
+from math import sqrt
+
+
+def sim_cos(prefs, p1, p2):
+    si = set(prefs[p1].keys()) & set(prefs[p2].keys())
+    if not si:
+        return 0
+    
+    sum_si = sum([(prefs[p1][it] * prefs[p1][it]) for it in si])
+    
+    sum1 = sum([pow(v, 2) for v in prefs[p1].values()])
+    sum2 = sum([pow(v, 2) for v in prefs[p2].values()])
+    return sum_si/sqrt(sum1*sum2)
