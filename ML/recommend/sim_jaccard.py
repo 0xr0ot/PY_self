efb7d788b18d0d93fddc04a1d7a84b7627cf8abe
Shinalone <shinalone@outlook.com>
2017-06-01 19:24:41 +0800

    Create sim_jaccard.py

---
+++ ML/recommend/sim_jaccard.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+#-*-coding: utf-8-*-
+
+def sim_jaccard(prefs, p1, p2):
+    si = set(prefs[p1].keys()) & set(prefs[p2].keys())
+    if not si:
+        return 0
+    sum_si = sum([(prefs[p1][it] * prefs[p1][it]) for it in si])
+    
+    sum1 = sum([pow(v, 2) for v in prefs[p1].values()])
+    sum2 = sum([pow(v, 2) for v in prefs[p2].values()])
+    
+    return sum_si/(sum1+sum2-sum_si)
