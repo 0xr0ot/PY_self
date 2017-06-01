bea58febec678f365160d0c353cde85814dc9a1b
Shinalone <shinalone@outlook.com>
2017-06-01 20:19:34 +0800

    Update sim_jaccard.py

--- ML/recommend/sim_jaccard.py
+++ ML/recommend/sim_jaccard.py
@@ -1,5 +1,10 @@
 #-*-coding: utf-8-*-
 
+'''
+Jaccard系数主要用于计算符号度量或布尔值度量的个体间的相似度，因为个体的特征属性都是由符号度量或者布尔值标识，因此无法衡量差异具体值的大小，
+只能获得“是否相同”这个结果，所以Jaccard系数只关心个体间共同具有的特征是否一致这个问题。
+'''
+
 def sim_jaccard(prefs, p1, p2):
     si = set(prefs[p1].keys()) & set(prefs[p2].keys())
     if not si:
@@ -9,5 +14,5 @@ def sim_jaccard(prefs, p1, p2):
     sum1 = sum([pow(v, 2) for v in prefs[p1].values()])
     sum2 = sum([pow(v, 2) for v in prefs[p2].values()])
     
-    r = sum_si/(sum1+sum2-sum_si)
+    r = sum_si/(sum1+sum2-sum_si) ## 交集除以并集
     return r
