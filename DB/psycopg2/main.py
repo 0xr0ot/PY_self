54e43dddf2723a96737ee9b6941934670c0b805f
UlionTse <shinalone@outlook.com>
2017-07-05 16:20:23 +0800

    Update main.py

--- DB/psycopg2/main.py
+++ DB/psycopg2/main.py
@@ -1,16 +1,25 @@
 import psycopg2
-
-conn = psycopg2.connect(database="qxiubase", user="xyl", password="xieyulonghenrenzhen", host="118.26.153.144", port="5432")
-cur = conn.cursor()
-cur.execute("insert ;")
-
+import datetime
 
 def save_psg(dt):
-    sql = ("INSERT INTO xyl.now_feedslist_v2 "
-            "(feeds_id,is_ad,jump_url,pic_url,preload_doodle_pic_url,preload_file_id,preload_pic_url,preload_video_url,recorder_nick,recorder_pic,recorder_uid,view_times,deadline) "
-            "VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}');".format(
-            dt['feeds_id'],dt['is_ad'],dt['jump_url'],dt['pic_url'],dt['preload_info']['doodle_pic_url'],dt['preload_info']['file_id'],
-            dt['preload_info']['pic_url'],dt['preload_info']['video_url'],dt['recorder_nick'],dt['recorder_pic'],dt['recorder_uid'],dt['view_times'],datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    sql = ("INSERT INTO ulion.feedslist (feeds_id,view_times,deadline) VALUES ('{0}','{1}','{2}');".format(
+            dt['feeds_id'],dt['view_times'],datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     cur.execute(sql)
     conn.commit()
     return 'Save successfully!'
+
+def main():
+    ##TODO
+    for dt in data:
+        #print(dt)
+        save_psg(dt)
+        #print('save successfully.')
+    return 'func_main finished!'
+
+if __name__ == '__main__':
+    conn = psycopg2.connect(database="db", user="ulion", password="xxxx", host="xxx.xx.xxx.xxx", port="5432")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE ulion.feedslist (feeds_id varchar PRIMARY KEY,view_times integer,deadline timestamp);")
+    print(main(30))
+    cur.close()
+    conn.close()
