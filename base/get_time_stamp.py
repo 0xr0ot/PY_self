253fde0b1090de42df125365b7590ba06ce41f1e
UlionTse <shinalone@outlook.com>
2017-07-05 16:26:13 +0800

    Update get_time_stamp.py

--- base/get_time_stamp.py
+++ base/get_time_stamp.py
@@ -1,6 +1,7 @@
 import time
 import datetime
 
+##method_1:
 def get_time_stamp():
     time_f = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     time_p = time.strptime(time_f, "%Y-%m-%d %H:%M:%S")
@@ -9,3 +10,6 @@ def get_time_stamp():
 
 if __name__ == '__main__':
     get_time_stamp()
+
+##method_2:
+time_stamp = int(time.time())
