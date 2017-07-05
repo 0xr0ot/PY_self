40c1f7582d488cdf11ded9212fa654c6d9aa3ebb
UlionTse <shinalone@outlook.com>
2017-07-05 17:00:55 +0800

    Update and rename get_time_stamp.py to trans_time_stamp.py

--- base/trans_time_stamp.py
+++ base/trans_time_stamp.py
@@ -2,14 +2,14 @@ import time
 import datetime
 
 ##method_1:
-def get_time_stamp():
+def get_timestamp():
     time_f = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     time_p = time.strptime(time_f, "%Y-%m-%d %H:%M:%S")
     time_stamp = int(time.mktime(time_p))
     return time_stamp
 
 if __name__ == '__main__':
-    get_time_stamp()
+    get_timestamp()
 
 ##method_2:
 time_stamp = int(time.time())
