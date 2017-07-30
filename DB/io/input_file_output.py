fff95562b16122e97f5b43d4d1b9b8cb6c72c0eb
UlionTse <shinalone@outlook.com>
2017-07-30 21:08:51 +0800

    Create input_file_output.py

---
+++ DB/io/input_file_output.py
@@ -0,0 +1,45 @@
+#!usr/bin/env python3
+#-*-coding: utf-8-*-
+
+
+import csv
+import numpy as np
+import pandas as pd
+
+path_0 = '/Users/qwe/test0.csv'
+path_1 = '/Users/qwe/test1.csv'
+items = [{'中原地产': 67}, {'我爱我家': 68}]
+
+
+
+def read_bigfile(path):
+    content = []
+    with open(path, 'r', encoding='utf-8') as f:
+        mark = True
+        while mark:
+            content.append((f.readline())[:-1]) ##del '\n'
+            mark = content[-1]
+    return content[:-1]
+
+
+def read_smallfile(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        content = f.readlines() ##has '\n'
+    return content
+        
+
+def write_file(path):
+    with open(path, 'w', encoding='utf-8') as f:
+        for item in items:
+            f.write(str(item))
+
+
+
+np_data = np.genfromtxt(path_0, skip_header=1, dtype=None, delimiter=',')
+print(np_data[0][0].decode('utf-8'))
+
+pd_data = pd.read_csv(path_0, header=0, sep=',', encoding='utf-8')
+#read_table
+pd_data.to_csv('test0_to_csv.xlsx', sep='\t', index=False, encoding='utf-8')
+
+#TODO
