020e75f674771f5da7961fd10e9c94152abbaca4
UlionTse <shinalone@outlook.com>
2017-09-16 22:56:48 +0800

    Update method_api_pool.py

--- base/ip2loc/method_api_pool.py
+++ base/ip2loc/method_api_pool.py
@@ -2,4 +2,4 @@ api_pool = ['http://ip.taobao.com/service/getIpInfo.php?ip=xxx.xxx.xx.xx',
             'http://api.chenjiehua.me/ip/xxx.xxx.xx.xx',
             'http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip=xxx.xxx.xx.xx',
             'http://freegeoip.net/json/xxx.xxx.xx.xx',
-            'http://ip138.com/ips138.asp?ip=xxx.xxx.xx.xx&action=2']
+            'http://ip138.com/ips138.asp?ip=xxx.xxx.xx.xx']
