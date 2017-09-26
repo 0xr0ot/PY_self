cb8a76f014819caff1fd47839d60efa8c1520827
UlionTse <shinalone@outlook.com>
2017-09-27 01:17:34 +0800

    Update and rename note1.py to reverseCipher.py

---
+++ data_encryption/reverseCipher.py
@@ -0,0 +1,12 @@
+message = 'Three can keep a secret, if two of them are dead.'
+
+
+translated = ''
+i = len(message) - 1
+
+while i > 0:
+    translated += message[i]
+    i -= 1
+    
+print(translated)
+#".daed era meht fo owt fi ,terces a peek nac eerh"
