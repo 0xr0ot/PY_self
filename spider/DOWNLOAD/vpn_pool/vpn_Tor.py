8f13abee09a2b9697ec174f13aa3dc49026742d9
UlionTse <shinalone@outlook.com>
2017-10-20 01:21:46 +0800

    Update vpn_Tor.py

--- spider/DOWNLOAD/vpn_pool/vpn_Tor.py
+++ spider/DOWNLOAD/vpn_pool/vpn_Tor.py
@@ -1,8 +1,17 @@
+# Method_1
 import socks
 import socket
 import requests
 
 socks.set_default_proxy(socks.SOCKS5,'localhost',9150)
 socket.socket = socks.socksocket
-
 print(requests.get('http://icanhazip.com').text)
+
+# Method_2
+from selenium import webdriver
+
+service_args = ['--proxy=localhost:9150', '--proxy-type=socks5']
+driver = webdriver.PhantomJS(executable_path='<path to PhantomJS>',service_args=service_args)
+driver.get('http://icanhazip.com')
+print(driver.page_source)
+driver.close()
