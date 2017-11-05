1f2f4a4934e7f36e7bb4553053ffd9a7020f9785
UlionTse <shinalone@outlook.com>
2017-11-05 22:58:24 +0800

    Update multiprocessing_note.py

--- base/multiprocessing_note.py
+++ base/multiprocessing_note.py
@@ -21,3 +21,19 @@ if __name__ == '__main__':
 #normal:0.1716
 #pool_map:0.37
 #pool_apply:3.046
+
+
+from multiprocessing import Process
+import os
+
+# 子进程要执行的代码
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
