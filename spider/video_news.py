7c713383fbcd778797db150a9e43eade7adaa1e1
UlionTse <shinalone@outlook.com>
2017-11-17 19:03:20 +0800

    Update video_news.py

--- spider/video_news.py
+++ spider/video_news.py
@@ -1,10 +1,13 @@
 #coding:utf-8
 #author:uliontse
 
+import re
 import time
 import requests
+from html import unescape
 from bs4 import BeautifulSoup
 from urllib.parse import urlencode
+
 
 class BaiduNews:
     def __init__(self):
@@ -29,26 +32,16 @@ class BaiduNews:
                       'BD_CK_SAM=1; PSINO=1; BDSVRTM=143; H_PS_PSSID='.format(self.tm1-82207,self.tm1-81353,self.tm1-798,self.tm1-798,self.tm0)
         }
 
-    def get_soup(self,keyword):
-        param = {
-            'ct':1,
-            'rn':20,
-            'ie':'utf-8',
-            'bs':keyword,
-            'rsv_bp':1,
-            'sr':0,
-            'cl':2,
-            'f':8,
-            'prevct':'no',
-            'tn':'newstitle',
-            'word':keyword
-        }
+
+    def get_index_soup(self,keyword):
+        param = {'ct':1, 'rn':20, 'ie':'utf-8', 'bs':keyword, 'rsv_bp':1,'sr':0, 'cl':2, 'f':8, 'prevct':'no',
+                 'tn':'newstitle', 'word':keyword}
         res = requests.get(self.host,params=urlencode(param),headers=self.headers)
         soup = BeautifulSoup(res.text,'lxml')
         return soup
 
 
-    def get_links(self,soup):
+    def get_index_links(self,soup):
         data = soup.find_all('h3', {'class': 'c-title'})
         for dt in data:
             yield {
@@ -57,45 +50,113 @@ class BaiduNews:
             }
 
 
-    def get_origin(self,soup):
+    def get_index_origin(self,soup):
         item = soup.find_all('div',{'class': 'c-title-author'})
         org_public = []
         org_date = []
         for it in item:
-            org = it.get_text().split('\xa0\xa0')
+            org = it.get_text().split('\xa0\xa0') #space
             org_public.append(org[0])
             org_date.append(org[1])
         org_dic = {'public': org_public, 'date': org_date}
         return org_dic
 
-    def get_content(self,link):
+
+    def get_html(self,link):
         res = requests.get(link)
-        res.encoding = 'utf-8'
-        soup = BeautifulSoup(res.text,'lxml')
-        if 'id="main_content"' in res.text:
-            contents = soup.find_all('div',{'id':'main_content'}) #凤凰网
-        elif 'id="contentMain"' in res.text:
-            contents = soup.find_all('div',{'id':'contentMain'}) #光明网
+        #print(res.text)
+        if 'charset="' in res.text:
+            pattern = re.compile('charset="(.*?)"',re.S)
+            res.encoding = re.findall(pattern,res.text)[0]
+        elif ('charset=gb2312' or 'charset=GB2312') in res.text:
+            res.encoding = 'gb2312'
+        elif ('charset=GBK' or 'charset=gbk') in res.text:
+            res.encoding = 'gbk'
+        elif 'charset=cp936' in res.text:
+            res.encoding = 'cp936'
         else:
-            return
-        para = ''
-        for content in contents[0].find_all('p'):
-            para = para + '\n' +content.get_text()
+            res.encoding = 'utf-8'
+        html = res.text
+        return html
+
+
+    def parse_choose(self,html):
+        soup = BeautifulSoup(html, 'lxml')
+
+        if 'id="main_content"' in html:
+            contents = soup.find_all('div',{'id':'main_content'}) #凤凰网
+            return contents[0].get_text()
+
+        elif ('articleInfo: {' in html) and ('&gt;&lt' in html): #头条
+            pattern = re.compile("content: '(.*?)'")
+            contents = re.findall(pattern,html)[0]
+            normal_text = BeautifulSoup(unescape(contents),'lxml')
+            return normal_text.get_text()
+
+        elif 'id="js_content"' in html:
+            contents = soup.find_all('div',{'id':'js_content'}) #微信文章
+            return contents[0].get_text()
+
+        elif 'id="artibody"' in html:
+            contents = soup.find_all('div',{"id":"artibody"}) #新浪
+            return contents[0].get_text()
+
+        elif 'class="article"' in html:
+            contents = soup.find_all('article',{'class':'article'}) #搜狐
+            return contents[0].get_text()
+
+        elif 'id="endText"' in html:
+            contents = soup.find_all('div',{'id':'endText'}) #网易
+            return contents[0].get_text()
+
+        elif 'id="contentMain"' in html:
+            contents = soup.find_all('div',{'id':'contentMain'}) #光明网
+            return contents[0].get_text()
+
+        elif 'id="content"' in html:
+            contents = soup.find_all('div',{'id':'content'}) #南方网、ITBEAR
+            return contents[0].get_text()
+
+        elif 'class="news-con"' in html:
+            contents = soup.find_all('div',{'class':'news-con'}) #大众网
+            return contents[0].get_text()
+        else:
+            return ''
+
+
+    def get_content(self,contents):
+        para = contents
         return {
-            'content': para,
-            'length': len(para.replace(' ',''))
+            'length': len(para.replace(' ', '')),
+            'content': para
         }
+
+
+def run():
+    news = BaiduNews()
+    #soup = news.get_index_soup('陌陌')
+    html = news.get_html(link10)
+    contents = news.parse_choose(html)
+    aa = news.get_content(contents)
+    print(aa)
 
 
 
 
 if __name__ == '__main__':
-    news = BaiduNews()
-    soup = news.get_soup('陌陌')
-    #print(news.get_origin(soup))
-    # for dt in news.get_links(soup):
+    #print(news.get_index_origin(soup))
+    # for dt in news.get_index_links(soup):
     #     print(dt)
-    link1 = 'http://e.gmw.cn/2017-11/16/content_26806647.htm'
-    link2 = 'http://tech.ifeng.com/a/20171115/44761603_0.shtml'
-    aa = news.get_content(link2)
-    print(aa)
+    link1 = 'http://e.gmw.cn/2017-11/16/content_26806647.htm' #光明网
+    link2 = 'http://tech.ifeng.com/a/20171115/44761603_0.shtml' #凤凰网
+    link3 = 'http://news.sina.com.cn/o/2017-07-25/doc-ifyihrwk2265523.shtml' #新浪
+    link4 = 'http://www.sohu.com/a/203545097_115980' #搜狐
+    link5 = 'http://ent.southcn.com/8/2017-11/10/content_178757607.htm' #南方网
+    link6 = 'http://www.itbear.com.cn/html/2017-11/244423.html' #ITBEAR
+    link7 = 'http://www.dzwww.com/yule/zy/201711/t20171109_16633719.htm' #大众网
+    link8 = 'http://tech.163.com/17/1117/13/D3ER9D5E00099504.html' #NetEase
+    link9 = 'https://www.toutiao.com/a6489226045797958158/' #toutiao
+    link10= 'https://mp.weixin.qq.com/s?__biz=MjM5MDI1ODUyMA==&mid=2672939627&idx=1&sn=14d587f0ccf8bf459406e3de1b189504&' \
+            'chksm=bce2f65c8b957f4a72acbccb0cba549898bb39a582a67265df5b502823e3a87e1fcb8202ca11&mpshare=1&scene=1&' \
+            'srcid=1117YeA0NLTDwKLvHM9kcnmQ&pass_ticket=W%2FN7x5QMxuwmbIEiD9OZsm%2BZ0U181Ugx3dgWlUW1OEJiHDVyRt5%2F8L4tbKWACFja#rd' #weixin
+    run()
