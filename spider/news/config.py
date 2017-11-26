c040252bbde9f6350e10a7b7d80bf2a5fe1166cb
UlionTse <shinalone@outlook.com>
2017-11-27 04:31:14 +0800

    Create config.py

---
+++ spider/news/config.py
@@ -0,0 +1,47 @@
+# coding:utf-8
+
+import re
+import requests
+from html import unescape
+from bs4 import BeautifulSoup
+from pprint import pprint as ppt
+
+PARSE_POOL = {
+    'id="main_content"': {'who':'ifeng','only':'http://www.ifeng.com/corp/feedback/'},
+    'id="Cnt-Main-Article-QQ"': {'who':'tencent_cnt','only':'http://www.qq.com/map/'},
+    'id="js_content"': {'who':'weixin','only':'res.wx.qq.com/mmbizwap/zh_CN'},
+    'id="articleContent"': {'who':'sina_auto','only':'http://corp.sina.com.cn/chn/sina_job.html'},
+    'id="endText"': {'who':'netease','only':'http://help.163.com/'},
+    'id="content-text"': {'who':'btime','only':'www.btime.com/aboutus.html'},
+    'class="content-bd"': {'who':'yidian','only':'tousu@yidian-inc.com'},
+    'class="article-content-wrap"': {'who':'huxiu','only':'static.huxiucdn.com/www/'},
+    'class="article-content"': {'who':'geekpark','only':'contact@geekpark.net'},
+    'id="contentMain"': {'who':'gmw','only':'http://en.gmw.cn/'},
+    'class="news-con"': {'who':'dzwww','only':'http://www.dzwww.com/map/'},
+    'class="page_text"': {'who':'youth','only':'http://news.youth.cn/gn/'},
+    'id="ctrlfscont"': {'who':'cankaoxiaoxi','only':'http://www.cankaoxiaoxi.com/about/'},
+    'id="p-detail"': {'who':'xinhuanet','only':'http://203.192.6.89/xhs/'},
+    'id="rwb_zw"': {'who':'people','only':'http://m.people.cn/'},
+    'class="left_zw"': {'who':'chinanews','only':'http://hr.chinanews.com/'},
+    'id="text"': {'who':'huanqiu','only':'http://mobile.huanqiu.com/#mobileBox'},
+    'id="FineDining"': {'who':'zaobao','only':'http://zproperty.zaobao.com.sg'},
+    'class="story-body"': {'who':'ftchinese','only':'http://www.ftchinese.com/m/corp/follow.html'},
+    'id="chan_newsDetail"': {'who':'china','only':'http://www.china.com/zh_cn/general/about.html'},
+    'class="detail"': {'who':'qdaily','only':'http://img.qdaily.com/user/'},
+    'class="left_main"': {'who':'ittime','only':'http://www.ittime.com.cn/newslist.shtml'},
+    'class="text-title-tip"': {'who':'szonline','only':'http://www.szonline.net/channel/'},
+    'class="news_txt"': {'who':'thepaper','p':'','only':'http://file.thepaper.cn/www/'},#p
+    'id="tdcontent"': {'who':'cfi','p':'','only':'http://vip.cfi.cn/'},#p
+    'class="article"': {'who': 'sohu', 'div': 'article', 'only': 'www.sohu.com/tag/'},#div
+    'class="article-cont clearfix"': {'who': 'zol', 'space':0,'only': 'news.zol.com.cn/more'},#space
+    'class="article-content fontSizeSmall BSHARE_POP"': {'who':'stnn','space': 0,'only':'http://www.stnn.cc/Copyright/ggfw/wzjs.html'},#space
+    'class="content all-txt"': {'who': 'guancha', 'space': 1,'only': 'http://www.guancha.cn/about/Copyright.shtml'},#space
+    'class="tpk_text clearfix"': {'who': 'takungpao', 'space': 0,'only': 'http://www.takungpao.com/corp/about.html'},#space
+    'id="artibody"': {'who': 'sina&&techweb','only': ['http://corp.sina.com.cn/chn/sina_job.html', 'http://www.techweb.com.cn/about.html']},#only
+    'class="content"': {'who': 'bjnews&&xiaohulu&&admin5','only': ['http://static.bjnews.com.cn/www/', 'kf@xiaohulu.com','http://www.admin5.com/about/']},#only
+    'id="content"': {'who': 'itbear&&southcn','only': ['http://m.itbear.com.cn/', 'http://www.newsgd.com/']},#only
+    'articleInfo: {': {'who': 'toutiao','only': 'pstatp.com/toutiao/static/js/'},#id-class
+    'article': {'who': 'qihoo','only': 'http://kc.look.360.cn'}#id-class
+}
+
+#'id="content clearfix"': {'who': 'itbear&&southcn','div':'article','p':'','space':1,'only': ['http://m.itbear.com.cn/', 'http://www.newsgd.com/']}
