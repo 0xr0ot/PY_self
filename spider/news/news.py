e05673ac0a0915c1c6440c84d463508d6f3231bd
UlionTse <shinalone@outlook.com>
2017-11-27 04:48:51 +0800

    Update news.py

--- spider/news/news.py
+++ spider/news/news.py
@@ -38,32 +38,32 @@ class News:
                       'BD_CK_SAM=1; PSINO=1; BDSVRTM=143; H_PS_PSSID='.format(self.tm1 - 82207, self.tm1 - 81353,
                                                                               self.tm1 - 798, self.tm1 - 798, self.tm0)
         }
-        self.conn = pymysql.connect(host='xxxx',
-                                    port=33066,
-                                    user='xxxx',
-                                    password='xxxx',
-                                    database='xxxx',
-                                    charset='utf8mb4',
-                                    cursorclass=pymysql.cursors.DictCursor)
-        self.cur = self.conn.cursor()
-        self.create_sql = '''
-                            CREATE TABLE IF NOT EXISTS qxiu_bi2.xyl__VideoNews_v1
-                                (
-                                      title VARCHAR(64) NOT NULL PRIMARY KEY
-                                    , crawlTime INT(10) NOT NULL
-                                    , keyword VARCHAR(8) NOT NULL 
-                                    , length INT(4) NOT NULL 
-                                    , publish VARCHAR(32) NOT NULL 
-                                    , pubDate VARCHAR(32) NOT NULL 
-                                    , word_TF VARCHAR(64) NULL 
-                                    , word_RANK VARCHAR(64) NULL 
-                                    , emotion VARCHAR(8) NULL
-                                    , image VARCHAR(256) NULL
-                                    , video VARCHAR(64) NULL 
-                                    , link VARCHAR(256) NOT NULL 
-                                    , content TEXT NULL
-                                );'''
-        self.cur.execute(self.create_sql)
+        # self.conn = pymysql.connect(host='xxxx',
+        #                             port=33066,
+        #                             user='xxxx',
+        #                             password='xxxx',
+        #                             database='xxxx',
+        #                             charset='utf8mb4',
+        #                             cursorclass=pymysql.cursors.DictCursor)
+        # self.cur = self.conn.cursor()
+        # self.create_sql = '''
+        #                     CREATE TABLE IF NOT EXISTS qxiu_bi2.xyl__VideoNews_v1
+        #                         (
+        #                               title VARCHAR(64) NOT NULL PRIMARY KEY
+        #                             , crawlTime INT(10) NOT NULL
+        #                             , keyword VARCHAR(8) NOT NULL
+        #                             , length INT(4) NOT NULL
+        #                             , publish VARCHAR(32) NOT NULL
+        #                             , pubDate VARCHAR(32) NOT NULL
+        #                             , word_TF VARCHAR(64) NULL
+        #                             , word_RANK VARCHAR(64) NULL
+        #                             , emotion VARCHAR(8) NULL
+        #                             , image VARCHAR(256) NULL
+        #                             , video VARCHAR(64) NULL
+        #                             , link VARCHAR(256) NOT NULL
+        #                             , content TEXT NULL
+        #                         );'''
+        # self.cur.execute(self.create_sql)
         self.web = '<!DOCTYPE HTML><html><head><title></title></head><body> </body></html>'
 
     def get_baidu(self, keyword, page=0):
@@ -127,10 +127,13 @@ class News:
               'Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11',
               'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5']
         UA.append(self.headers['User-Agent'])
-        res = requests.get(link, headers={'User-Agent': random.choice(UA)})
-
-        res.encoding = self.encode_bug(res)
-        html = res.text
+        try:
+            global res
+            res = requests.get(link, headers={'User-Agent': random.choice(UA)})
+            res.encoding = self.encode_bug(res)
+            html = res.text
+        except:
+            html = self.web
         soup = BeautifulSoup(html, 'lxml')
         for parser in PARSE_POOL.keys():
             if (parser == 'articleInfo: {' and self.judge(PARSE_POOL[parser]['only'], html)):
@@ -248,9 +251,9 @@ class News:
 
 
 if __name__ == '__main__':
-    items = ['直播行业', '小视频', '短视频', '网红 直播', '小米直播', '人人直播', '陌陌', '映客', '花椒直播', '奇秀直播', '一直播', 'NOW直播',
-             '六间房直播', '来疯', '千帆直播', '我秀直播', '繁星直播', '网易cc直播', '网易BoBo直播', '网易薄荷直播', '全民直播', '花样直播',
-             'YY直播', 'live直播', 'vlive直播', 'KK直播', '梦想直播', '聚星直播', '新浪秀场','豆豆Live',
+    items = ['直播行业', '小视频', '短视频', '网红 直播', '小米直播', '全民直播', '陌陌', '映客', '花椒直播', '奇秀直播', '一直播', 'NOW直播',
+             '六间房直播', '来疯', '千帆直播', '我秀直播', '繁星直播', '网易cc直播', '网易BoBo直播', '网易薄荷直播', '花样直播',
+             'YY直播', 'live直播', 'vlive直播', 'KK直播', '梦想直播', '聚星直播', '新浪秀场','豆豆Live','人人直播',
              '美拍', '快手', '火山小视频', '抖音', '淘宝直播', '梨视频', '开眼视频', '秒拍', '映兔视频', 'V电影', '魔力盒',
              '快视频', '西瓜视频',
              '熊猫直播', '斗鱼直播', '虎牙直播', '企鹅直播', '企鹅电竞', '熊猫直播', '战旗直播', '狮吼直播', '触手直播', '龙珠直播']
@@ -266,7 +269,7 @@ if __name__ == '__main__':
                 if (('怎么用' or '好用吗' or '直播：') in dt['title']) or dt['length']<500:
                     pass
                 else:
-                    news.save_data(dt)
+                    #news.save_data(dt)
                     print('Saved!')
     finally:
         news.save_end()
