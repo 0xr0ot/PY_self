e533fa46b17074acc3c9fdd37c07d45b026541f4
UlionTse <shinalone@outlook.com>
2018-03-03 21:22:11 +0800

    Update maxAvg.py

--- algorithm/others/maxAvg.py
+++ algorithm/others/maxAvg.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-def maxAverage(nums, k):
+def maxAverage1(nums, k):
     '求列表中大于等于k个长度的子列表的最大平均值。'
     # O(n**2)
     r = []
@@ -10,5 +10,16 @@ def maxAverage(nums, k):
     print(r)
     return max(r)
 
+    def maxAverage2(nums, k):
+        # update `z`,`r1`.
+        z = len(nums)
+        r1 = sum(nums[:k])/k
+        for n in range(k,z+1):
+            for i in range(z - n + 1):
+                r2 = sum(nums[i:i + n]) / n
+                if r1 < r2: r1 = r2
+        return r1
+
+    
 if __name__ == '__main__':
     print(maxAverage([-1,-2,-3,-100,-1,-50],4))
