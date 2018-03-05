c660af1381053914578efb09309aa755a372d90a
UlionTse <shinalone@outlook.com>
2018-03-05 19:26:05 +0800

    Update quick_sort.py

--- algorithm/sort/quick_sort.py
+++ algorithm/sort/quick_sort.py
@@ -1,8 +1,7 @@
 def quick_sort(L):
-    if len(L) < 2:
-        return L
-    else:
+    if len(L) > 1:
         q = L[0]
         less = quick_sort([elem for elem in L if elem < q])
         more = quick_sort([elem for elem in L if elem > q])
         return less + [q] * L.count(q) + more
+    return L
