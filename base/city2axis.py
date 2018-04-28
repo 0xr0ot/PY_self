9b90e7a3c0ef8c4909917c0573ca5873d3dc6864
UlionTse <shinalone@outlook.com>
2018-04-28 20:17:17 +0800

    Update city2axis.py

--- base/city2axis.py
+++ base/city2axis.py
@@ -3,11 +3,20 @@
 
 import requests
 
-def getMap(city):
-    key = 'GjG3XAdmywz7CyETWqHwIuEC6ZExY6QTxyl'
+def baiduMap(city):
+    key = 'GjG3XAdmywz7CyETWqHwIuEC6ZExY6QT'
     url = 'http://api.map.baidu.com/geocoder/v2/?output=json&ak={}&address='.format(key) + city
     data = requests.get(url).json()
-    # if data['status'] == 1:
-    #     print('{0}: {1}'.format(city,data['msg']))
-    #     return data
     return data
+
+def googleMap(city):
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city
+    data = requests.get(url).json()
+    return data
+
+
+for city in ('上海','上海市','纽约','New York','Bixessarri','NULL',''):
+    # print(baiduMap(city))
+    print(googleMap(city))
+    # 申请key
+    # https://console.developers.google.com/apis/credentials?project=_
