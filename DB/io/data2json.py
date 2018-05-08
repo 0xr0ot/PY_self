b63fa350fc3bb407e6ca754018b4a9bf492844ec
UlionTse <shinalone@outlook.com>
2018-05-08 19:53:50 +0800

    Update data2json.py

--- DB/io/data2json.py
+++ DB/io/data2json.py
@@ -6,11 +6,11 @@ data = {
     'FlyLine': ['beijing','shanghai','123'],
     'latlng': [30,-100]
 }
-
+## save:
 with open("/path/data.json",'w',encoding='utf-8') as json_file:
     json.dump(data,json_file,ensure_ascii=False) #不保证英文编码
 
-    
+## read:   
 with open(r"C:\path\data.json",'r',encoding='utf-8') as json_file:
     data = json.load(json_file)
     print(data.keys())
