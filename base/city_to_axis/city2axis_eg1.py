62b31faa92f938b7c93ad7f38725e643811df454
UlionTse <shinalone@outlook.com>
2018-05-12 17:37:14 +0800

    Update city2axis_eg1.py

--- base/city_to_axis/city2axis_eg1.py
+++ base/city_to_axis/city2axis_eg1.py
@@ -2,9 +2,10 @@
 # uliontse
 
 import requests
-import pandas as pd
+# import pandas as pd
 
-path = r'C:\{your_path}\city.txt'
+path_r = r'C:\{your_path}\city.txt'
+path_r = r'C:\{your_path}\city_latlng.txt'
 
 def googleMap(city):
     error_data = {'lat': 404, 'lng': 404}
@@ -33,19 +34,38 @@ def read_bigfile(path):
             mark = content[-1]
     return content[:-1]##list
 
-city_pool = read_bigfile(path)
+def main():
+    with open(path_r,'r',encoding='utf-8') as f1:
+        city = True
+        n = 1
+        with open(path_w, 'a', encoding='utf-8') as f2:
+            while city:
+                city = f1.readline()[:-1]
+                if n < 993:
+                    # data = googleMap(city)
+                    # data = mapQuest(city)
+                    data = baiduMap(city)
+                    line = ';'.join([str(n), city, str(round(data['lat'],4)), str(round(data['lng'],4))])
+                    print(line)
+                    f2.write(line + '\n')
+                n += 1
 
-# new_city = []
-lat_pool, lng_pool = [], []
-for city in city_pool:
-    data = googleMap(city)
-    print(city, data)
-    lat_pool.append(data['lat'])
-    lng_pool.append(data['lng'])
-    # new_city.append(city.strip())
+if __name__ == '__main__':
+    main()
 
-print(len(city_pool),len(lat_pool),len(lng_pool))
+# city_pool = read_bigfile(path)
 
-pool = [city_pool,lat_pool,lng_pool]
-df = pd.DataFrame(columns=['city','lat','lng'],data=pool)
-df.to_csv(r'C:\{your_path}\city_axis.csv')
+# # new_city = []
+# lat_pool, lng_pool = [], []
+# for city in city_pool:
+#     data = googleMap(city)
+#     print(city, data)
+#     lat_pool.append(data['lat'])
+#     lng_pool.append(data['lng'])
+#     # new_city.append(city.strip())
+
+# print(len(city_pool),len(lat_pool),len(lng_pool))
+
+# pool = [city_pool,lat_pool,lng_pool]
+# df = pd.DataFrame(columns=['city','lat','lng'],data=pool)
+# df.to_csv(r'C:\{your_path}\city_axis.csv')
