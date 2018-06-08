90f63c082d02b456b8a68b40fb63fc8b9a30b538
UlionTse <shinalone@outlook.com>
2018-06-09 01:05:34 +0800

    Update judgeChinese.py

--- base/judgeChinese.py
+++ base/judgeChinese.py
@@ -1,6 +1,14 @@
-def judgeChinese(word):
+def judgeChinese(word:str,punctuation_filter=True):
+    English_punctuation = '''`~!@#$%^&*()_+-=[]\{}|;':",./<>?'''
+    Chinese_punctuation = '''·~！@#￥%……&*（）——+-=【】、{}|；‘’：“”，。《》？'''
     n = 0
     for per_word in word:
         if u'\u4e00' <= per_word <= u'\u9fff':
             n += 1
+        elif punctuation_filter is True:
+            if per_word in Chinese_punctuation:
+                n += 1
+        else:
+            if per_word in set(Chinese_punctuation + English_punctuation):
+                n += 1
     return n == len(word)
