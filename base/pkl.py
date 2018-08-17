dd5f3cbf12528b78ea080ecf35a027e086de94fc
UlionTse <shinalone@outlook.com>
2018-08-18 01:00:50 +0800

    Update pkl.py

--- base/pkl.py
+++ base/pkl.py
@@ -12,3 +12,14 @@ with open('svm_model_iris.pkl', 'wb') as f:
 with open('svm_model_iris.pkl', 'rb') as f:
     model = pickle.load(f)
     print(model)
+
+#########################################
+import numpy as np
+import pandas as pd
+
+
+df =  pd.DataFrame(np.arange(24).reshape(4,6))
+print(df)
+df.to_pickle('df.pkl')
+df2 = pd.read_pickle('df.pkl')
+print(df2)
