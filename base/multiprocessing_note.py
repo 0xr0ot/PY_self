61e697cb1974a1948993659649b0bbf1ec84cb18
UlionTse <shinalone@outlook.com>
2018-12-07 16:12:11 +0800

    Update multiprocessing_note.py

--- base/multiprocessing_note.py
+++ base/multiprocessing_note.py
@@ -1,39 +1,40 @@
 import time
 from multiprocessing import Pool
 
-def main(x):
-    pl = []
-    pl.append(x)
-    print(len(pl),x)
+def main(name):
+    print(name)
     
                   
 if __name__ == '__main__':
-    
+    name_list = ['Frank','Fiona','Lip','Ian','Debbie','Carl','Liam']
     t1 = time.time()
-    with Pool(4) as p:
-        #for i in range(12345):
-            #p.apply(main,(i,))
-        p.map(main,range(120))
-        #main(i)
+    
+    p = Pool(4) #python2 不能使用 with...as...写法
+    p.map(main,name_list)
+    p.terminate()
+    
     t2 = time.time()
     print(t2-t1)
+
+    #python2: multiprocessing.cpu_count()
+    #python3: os.cpu_count()
     
-#normal:0.1716
-#pool_map:0.37
-#pool_apply:3.046
+    
+    
+    
+    
 
+# from multiprocessing import Process
+# import os
 
-from multiprocessing import Process
-import os
+# # 子进程要执行的代码
+# def run_proc(name):
+#     print('Run child process %s (%s)...' % (name, os.getpid()))
 
-# 子进程要执行的代码
-def run_proc(name):
-    print('Run child process %s (%s)...' % (name, os.getpid()))
-
-if __name__=='__main__':
-    print('Parent process %s.' % os.getpid())
-    p = Process(target=run_proc, args=('test',))
-    print('Child process will start.')
-    p.start()
-    p.join()
-    print('Child process end.')
+# if __name__=='__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Process(target=run_proc, args=('test',))
+#     print('Child process will start.')
+#     p.start()
+#     p.join()
+#     print('Child process end.')
